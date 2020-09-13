a84b5b8d8b2fd22c208804c397c5501c11358fc8
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-09-13 14:25:00 +0100

    Created Player class
    Created a Player class to control eithera manual or automated player.

---
+++ BJSS/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BJSS
+{
+  public class Player
+  {
+    private bool _auto;
+
+    /// <summary>
+    /// Create a new player
+    /// </summary>
+    /// <param name="auto">true if controller by the "AI"</param>
+    public Player(bool auto)
+    {
+      _auto = auto;
+    }
+
+    /// <summary>
+    /// Make an intelligent move
+    /// </summary>
+    /// <returns>The card to play</returns>
+    public Card MakeSmartMove()
+    {
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns>The card to play</returns>
+    public Card MakeFirstMove()
+    {
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Make a manual move
+    /// </summary>
+    /// <returns>The card to play</returns>
+    public Card MakePlayerMove()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
