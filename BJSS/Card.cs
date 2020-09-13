7baf75f690ad47cd357c27c02770746f5789d3e4
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-09-13 13:52:50 +0100

    Created Card class
    Created a class to model a Card.

---
+++ BJSS/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BJSS
+{
+  public enum House
+  {
+    eSpades = 0,
+    eClubs,
+    eHearts,
+    eDiamonds
+  }
+
+  public enum Number
+  {
+    eTwo = 2,
+    eThree,
+    eFour,
+    eFive,
+    eSix,
+    eSeven,
+    eEight,
+    eNine,
+    eJack,
+    eQueen,
+    eKing,
+    eAce
+  }
+  public class Card
+  {
+    private House _house;
+    private Number _number;
+    public Card(House house, Number card)
+    {
+      _house = house;
+      _number = card;
+    }
+  }
+}
