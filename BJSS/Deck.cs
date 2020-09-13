5129985aec190445ad00b4780211398dddb73f51
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2020-09-13 13:59:47 +0100

    Create Deck.cs
    Created a Deck class to model a deck of cards.

---
+++ BJSS/Deck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BJSS
+{
+  public class Deck
+  {
+    private Card[] _deck;
+    public Deck()
+    {
+      _deck = new Card[52];
+    }
+  }
+}
